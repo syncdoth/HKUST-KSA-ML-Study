d0089d70c47bd4aee9bbc638026b83209e8dcad4
Choi Sehyun <choisehyun98@gmail.com>
2020-10-06 15:04:42 +0900

    add oop course

---
+++ oop/main.py
@@ -0,0 +1,9 @@
+from testing import test
+
+def main(arg):
+
+    t = test(arg)
+    t.debug()
+
+if __name__ == "__main__":
+    main(1)
---
+++ oop/testing.py
@@ -0,0 +1,6 @@
+class test:
+    def __init__(self, a):
+        self.a = a
+
+    def debug(self):
+        print(self.a)
