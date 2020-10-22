4bf2eca256753cb929d89f591bf4fb573275eff3
Choi Sehyun <schoiaj@connect.ust.hk>
2020-10-22 22:15:01 +0900

    After Class Update #2

--- oop/main.py
+++ oop/main.py
@@ -1,4 +1,10 @@
-from testing import test
+"""The solution to the ModuleNotFoundError during class:
+
+move the testing_module folder inside the oop folder. Since the testing_module
+folder was outside of oop folder, main.py could not find the testing_module.
+"""
+
+from testing_module.testing import test
 
 def main(arg):
 
---
+++ oop/testing_module/__init__.py
@@ -0,0 +1,7 @@
+"""
+__init__.py is a file that has to be created for a folder to be considered a
+module.
+
+More Reference on modules can be found here:
+https://docs.python.org/3/tutorial/modules.html
+"""
